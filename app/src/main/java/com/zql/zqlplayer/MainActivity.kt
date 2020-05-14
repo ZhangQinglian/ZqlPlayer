836c1e3d1c860c40b19fd5760e1508f552c93b68
番茄沙司 <zhangqinglian@bilibili.com>
2020-05-14 14:38:04 +0800

    ui optimize

--- app/src/main/java/com/zql/zqlplayer/MainActivity.kt
+++ app/src/main/java/com/zql/zqlplayer/MainActivity.kt
@@ -5,6 +5,12 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import com.zql.zqlplayer.test.EGLTestActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,9 +20,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         requestPermissions()
-        btn_to_egl_test.setOnClickListener {
-            startActivity(Intent(applicationContext, EGLTestActivity::class.java))
+        initMenus()
+    }
+
+    private fun initMenus() {
+        val adapter = MenuListAdapter()
+        adapter.menuCallback = object : MenuListAdapter.MenuCallback {
+            override fun onMenuClick(position: Int) {
+                when (position) {
+                    0 -> {
+                        startActivity(Intent(applicationContext, EGLTestActivity::class.java))
+                    }
+                }
+            }
         }
+        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        menu_list.adapter = adapter
+        menu_list.layoutManager = layoutManager
+        adapter.notifyDataSetChanged()
     }
 
     private fun requestPermissions() {
@@ -27,8 +48,45 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
-    companion object {
+    class MenuListAdapter : RecyclerView.Adapter<MenuHolder>() {
 
+        interface MenuCallback {
+            fun onMenuClick(position: Int)
+        }
+
+        private val menus = arrayListOf("EGL 环境测试")
+
+        var menuCallback: MenuCallback? = null
+        override fun onCreateViewHolder(view: ViewGroup, position: Int): MenuHolder {
+
+            val view = LayoutInflater.from(view.context).inflate(R.layout.item_menu, view, false)
+            return MenuHolder(view, menuCallback)
+        }
+
+        override fun getItemCount(): Int = menus.size
+
+        override fun onBindViewHolder(holder: MenuHolder, position: Int) {
+            holder.bind(menus[position])
+        }
+
+    }
+
+    class MenuHolder(view: View, private val callback: MenuListAdapter.MenuCallback?) :
+
+        RecyclerView.ViewHolder(view) {
+
+        fun bind(menuText: String) {
+            itemView.findViewById<TextView>(R.id.menu_text)?.apply {
+                text = menuText
+            }
+            itemView.setOnClickListener {
+                callback?.onMenuClick(adapterPosition)
+
+            }
+        }
+    }
+
+    companion object {
         init {
             System.loadLibrary("native-lib")
         }
